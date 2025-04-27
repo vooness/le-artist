94f9f0f205cd8991ef04f2594f450022bfaf42a4
vooness <vooness@stream.cz>
2025-04-27 22:44:58 +0200

    update

--- src/app/components/WhatIDo.tsx
+++ src/app/components/WhatIDo.tsx
@@ -75,7 +75,7 @@ const IntroSection = () => {
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path>
         </svg>
       ),
-      link: "/sluzby/grafik"
+      link: "/sluzby/Grafika"
     },
     {
       id: 2,
